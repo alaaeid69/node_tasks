5165e84961724182ccea2ce4a8ef7bfaedb7f2db
alaaeid69 <alaaeid69.com>
2023-04-20 04:13:05 -0700

    node.js

---
+++ first.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app =express ();
+
+const students =[
+    "alaa",
+    "eid",
+    "amr",
+    "abeer",
+    "hana",
+    "amina"
+];
+const studentFunction =(req , res ) =>{
+    let out ="<ul>";
+    for ( let i = 0; i <students.length ;i++){
+     const student =students[i];
+     out +="<li>" + student + "</li>";
+   }
+   out+= "</ul>";
+   res.send(out)
+
+};
+app.get('/students', studentFunction);
+app.listen(3000);
